59fe36a4c159b98a1640486851ec992a91c14c24
marklookermans <77417280+marklookermans@users.noreply.github.com>
2024-12-23 15:15:55 +0100

    Update __init__.py

--- homeassistant/components/hp_ilo/__init__.py
+++ homeassistant/components/hp_ilo/__init__.py
@@ -1,1 +1,21 @@
 """The HP Integrated Lights-Out (iLO) component."""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up HP iLO from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
